829952ac7d595a21d21398574ce3a56eb28d9eb3
Rui Li <ruiamzn@amazon.com>
2025-10-31 00:59:48 +0000

    Revert "8355746: Start of release updates for JDK 26"
    This reverts commit af87035b713f8bfe05a007a4d4670cefc6a6aaf2.

--- src/java.compiler/share/classes/javax/lang/model/SourceVersion.java
+++ src/java.compiler/share/classes/javax/lang/model/SourceVersion.java
@@ -468,18 +468,6 @@ public enum SourceVersion {
      * JEP 513: Flexible Constructor Bodies</a>
      */
     RELEASE_25,
-
-    /**
-     * The version introduced by the Java Platform, Standard Edition
-     * 26.
-     *
-     * @since 26
-     *
-     * @see <a
-     * href="https://docs.oracle.com/javase/specs/jls/se26/html/index.html">
-     * <cite>The Java Language Specification, Java SE 26 Edition</cite></a>
-     */
-    RELEASE_26,
     ; // Reduce code churn when appending new constants
 
     // Note that when adding constants for newer releases, the
@@ -489,7 +477,7 @@ public enum SourceVersion {
      * {@return the latest source version that can be modeled}
      */
     public static SourceVersion latest() {
-        return RELEASE_26;
+        return RELEASE_25;
     }
 
     private static final SourceVersion latestSupported = getLatestSupported();
@@ -504,7 +492,7 @@ public enum SourceVersion {
     private static SourceVersion getLatestSupported() {
         int intVersion = Runtime.version().feature();
         return (intVersion >= 11) ?
-            valueOf("RELEASE_" + Math.min(26, intVersion)):
+            valueOf("RELEASE_" + Math.min(25, intVersion)):
             RELEASE_10;
     }
 
