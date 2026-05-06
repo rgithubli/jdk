691496dece8cdc9966b74b31bd8d9a0c3e024334
Rui Li <ruiamzn@amazon.com>
2026-05-06 19:02:12 +0000

    Revert "8370890: Start of release updates for JDK 27"
    This reverts commit c55287d197ef024033f8dfbb5a365cb091bc67fb.

--- src/java.base/share/classes/java/lang/reflect/ClassFileFormatVersion.java
+++ src/java.base/share/classes/java/lang/reflect/ClassFileFormatVersion.java
@@ -383,18 +383,6 @@ public enum ClassFileFormatVersion {
      * <cite>The Java Virtual Machine Specification, Java SE 26 Edition</cite></a>
      */
     RELEASE_26(70),
-
-    /**
-     * The version introduced by the Java Platform, Standard Edition
-     * 27.
-     *
-     * @since 27
-     *
-     * @see <a
-     * href="https://docs.oracle.com/en/java/javase/27/docs/specs/jvms/index.html">
-     * <cite>The Java Virtual Machine Specification, Java SE 27 Edition</cite></a>
-     */
-    RELEASE_27(71),
     ; // Reduce code churn when appending new constants
 
     // Note to maintainers: when adding constants for newer releases,
@@ -410,7 +398,7 @@ public enum ClassFileFormatVersion {
      * {@return the latest class file format version}
      */
     public static ClassFileFormatVersion latest() {
-        return RELEASE_27;
+        return RELEASE_26;
     }
 
     /**
